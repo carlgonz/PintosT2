697bc3acf80c54579d5cbf01a87019e9d521872e
Carlos <carlosjimy@gmail.com>
2011-12-24 00:04:30 -0300

    Archivos cargados

---
+++ src/userprog/syscall.c
@@ -0,0 +1,188 @@
+#include "userprog/syscall.h"
+
+static struct lock syscall_lock;
+static void syscall_handler (struct intr_frame *);
+static bool is_valid_arg (void *arg);
+static void halt(void);
+static void exit(int status);
+static int write(int fd, const void  *buffer, unsigned size);
+static tid_t exec(const char  *cmd_line);
+static int wait(tid_t pid);
+
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+	lock_init(&syscall_lock);
+}
+
+static void
+syscall_handler (struct intr_frame *f) 
+{
+	int call;
+	int status;
+	
+	unsigned int f_des;
+	char *buffer;
+	unsigned int size;
+	int i;
+	
+	if(is_valid_arg(f->esp))
+		call = *(int*)(f->esp);
+	else
+		call = -1; /* Se ira al default del switch */
+		
+	/**/printf ("SYSCALL\n");
+	switch(call)
+	{
+		case SYS_HALT:
+		/**/printf ("SYS_HALT\n");
+			halt();
+			break;
+
+		case SYS_EXIT:
+			/* Revisar puntero */
+			if(is_valid_arg(f->esp+4))
+				status = *(int *)(f->esp + 4); /* Valido */
+			else
+				status = -1; /* Error */
+			
+			exit(status);
+			break;
+
+		case SYS_EXEC:
+			/* Revisar puntero */
+			if(is_valid_arg(f->esp+4))
+			{
+				status = exec((*(char **)(f->esp + 4)));; /* Valido */
+				f->eax = status;
+			}
+			else
+			{
+				status = -1;
+				exit(status);
+			}
+			/*Hay que hacer un llamado a Exec con el cmd se deberia encontrar en el esp, 
+			 no estoy seguro si es en el lugar que se encuentra el esp o el primero, asi como f->esp*/
+			break;
+		
+		case SYS_WAIT:
+			if(is_valid_arg(f->esp+4))
+			{
+				status = wait(*(tid_t *)(f->esp + 4)); /* Valido */
+				/* Retorno codigo del wait */
+				f->eax = status;
+			}
+			else
+			{
+				status = -1; /* Error */
+				exit(status);
+			}
+			break;
+			
+		case SYS_WRITE:		
+			/**/printf ("SYS_WRITE\n");
+			/* Verificar punteros */
+			//for(i=4;i<0x0c;i+=4)
+			//{
+			//	if(!is_valid_arg(f->esp+i))
+			//	{
+			//		exit(-1);
+			//		return;
+			//	}
+			//}
+			
+			/* Estando comprobados dereferencio los punteros */
+			f_des = *(int *)f->esp+4;
+			buffer = (void *)f->esp+8;
+			size = *(int *)f->esp+12;
+			
+			f->eax = write(f_des, buffer,size); /* Retorna la cantidad de bytes escritos */
+			break;
+		
+		default:
+			printf ("INVALID SYSCALL\n");
+			exit (-1);
+
+	}
+}
+
+
+/*SYS_HALT:*/
+static void 
+halt(void)
+{
+  power_off();
+}
+
+/* SYS_EXIT */
+static void 
+exit(int status)
+{
+	/* TODO: Liberar los recursos extras del thread */
+	struct thread *t;
+	t = thread_current();
+	//t->ret_status = status;			
+	// Se imprime mensaje de salida
+	printf("%s: exit(%d)\n",t->name, status);
+	thread_exit ();
+}
+
+/* SYS_WRITE */
+static int 
+write(int fd, const void  *buffer, unsigned size)
+{
+	if(fd == STDOUT_FILENO) /* Solo se implemente STDOUT_FILENO */
+			{	
+			
+			/* Se escribe en la consola sicronnizando con un mutex */	
+			lock_acquire(&syscall_lock);
+				putbuf(buffer,size);
+			lock_release(&syscall_lock);
+			}
+			else /* De otra forma se retorna 0 */
+			{
+				size = 0;
+			}
+			return size;
+}
+
+/* SYS_EXEC */
+static tid_t 
+exec(const char  *cmd_line)
+{
+	/* Uso el proccess_execute de process.c y lo confirmo*/
+	/* Agrego sincronizacion con mutex */
+	lock_acquire(&syscall_lock);
+		tid_t tid = process_execute (cmd_line);
+	lock_release(&syscall_lock);
+	if (tid == TID_ERROR) 
+		return -1;
+	else return tid;
+}
+
+/* SYS_WAIT */
+static int
+wait(tid_t pid)
+{
+	/* Se llama a process_wait */
+	return process_wait(pid);
+}
+
+/*------------------------------------------------------------------------------
+ * IS_VALID_ARG: Revisa que un puntero entregado como parámetro no sea un
+ * puntero nulo, un puntero a memoria virtual no mapeada o un puntero a
+ * espacio de direcciones del kernel.
+ *----------------------------------------------------------------------------*/ 
+static bool 
+is_valid_arg(void *arg)
+{
+	bool value = true;
+	if(	arg == NULL																								/* Nulo */
+		||!is_user_vaddr(arg)																				/* Kernel */
+	 	||pagedir_get_page(thread_current()->pagedir, arg) == NULL) /* No mapped */
+		value = false;
+	 
+	 return value;
+}
---
+++ src/userprog/syscall.h
@@ -0,0 +1,24 @@
+#ifndef USERPROG_SYSCALL_H
+#define USERPROG_SYSCALL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+
+#include "threads/init.h"
+#include "threads/vaddr.h"
+#include "threads/synch.h"
+#include "userprog/pagedir.h"
+#include "userprog/process.h"
+
+
+#include "filesys/filesys.h"
+#include "filesys/file.h"
+#include "threads/malloc.h"
+
+//#include "devices/shutdown.h"
+
+void syscall_init (void);
+#endif /* userprog/syscall.h */
